016b4e6132fbd2559fe38a8a12432e56b824637e
Huit-8 <eitoao88@gmail.com>
2023-07-01 23:38:43 +0900

    add takePhoto method

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.cameraapp
 
+import android.content.ContentValues
 import android.media.Image
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.example.cameraapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+
     private lateinit var binding: ActivityMainBinding
+
     //写真をとるために必要な変数を定義
     private var imageCapture: ImageCapture? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,16 +34,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         startCamera()
+
+        binding.imageCaptureButton.setOnClickListener{
+            takePhoto()
+        }
     }
 
     //カメラの画像を画面に出すメソッドを作る
-    private fun startCamera(){
+    private fun startCamera() {
         //カメラを使うためのデータを取得
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
             //ライフサイクルにカメラを使うための値を宣言
-            val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
             val preview = Preview.Builder().build().also {
                 it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
@@ -47,12 +63,47 @@ class MainActivity : AppCompatActivity() {
             try {
                 cameraProvider.unbindAll()
                 cameraProvider.bindToLifecycle(
-                    this,cameraSelector,preview,imageCapture
+                    this, cameraSelector, preview, imageCapture
                 )
-            }catch (exc: Exception){
-                Log.e("CameraX-sample","カメラの起動に失敗しました",exc)
+            } catch (exc: Exception) {
+                Log.e("CameraX-sample", "カメラの起動に失敗しました", exc)
             }
 
-        },ContextCompat.getMainExecutor(this))
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        //imageCaptureにデータがないときはメソッドを終了
+        val imageCapture = imageCapture ?: return
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-image")
+            }
+        }
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exception: ImageCaptureException) {
+                }
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(baseContext, "写真を保存しました", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+
+
     }
 }
